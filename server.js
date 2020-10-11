f13187d23f5de16f20d3f243334188a320d051ba
khermalik <khermalik@gmail.com>
2020-10-10 21:25:54 -0700

    set view engine and connected to root route

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = 4000;
 
+//Set View Engine
+app.set('view engine', 'ejs');
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+
+// Root Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // Listener
 app.listen(PORT, () => console.log(`Server started successfully on port ${PORT}`));
